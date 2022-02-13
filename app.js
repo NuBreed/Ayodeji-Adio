53867b991ab5833d5a0ab4f30650461c2f950722
NuBreed <ayodejiadio@gmail.com>
2022-02-13 15:12:32 +0100

    first version

---
+++ app.js
@@ -0,0 +1,22 @@
+const toggleBtn = document.querySelector('.fa-bars')
+const navbar = document.querySelector('.nav-links')
+
+toggleBtn.addEventListener('click', function() {
+    navbar.classList.toggle('show')
+})
+
+// back to top btn
+const topBtn = document.querySelector('.fa-arrow-up')
+
+window.addEventListener('scroll', function() {
+    if (this.window.scrollY > 900) {
+        topBtn.style.transform = 'translateX(0%)'
+        topBtn.addEventListener('click', function() {
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth',
+            })
+        })
+    }
+})
